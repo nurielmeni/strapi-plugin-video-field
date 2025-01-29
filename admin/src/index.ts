95fefbbeb5c44b249ce177d1db0a5c836f3eaa85
Nuriel Meni <nurielmeni@gmail.com>
2025-01-29 23:35:47 +0200

    bootstrap

--- admin/src/index.ts
+++ admin/src/index.ts
@@ -4,6 +4,48 @@ import { Initializer } from './components/Initializer';
 import { PLUGIN_ID } from './pluginId';
 import { prefixPluginTranslations } from './utils/prefixPluginTranslations';
 
+interface StrapiAdminAPI {
+    addMenuLink: (config: MenuLinkConfig) => void;
+    registerPlugin: (config: PluginConfig) => any;
+    createSettingSection: (section: SettingSectionConfig, links: SettingLinkConfig[]) => void;
+    getPlugin: (pluginId: string) => any;
+}
+
+interface MenuLinkConfig {
+    to: string;
+    icon: React.ComponentType;
+    intlLabel: {
+        id: string;
+        defaultMessage: string;
+    };
+    Component: () => Promise<any>;
+}
+
+interface PluginConfig {
+    id: string;
+    initializer: React.ComponentType<{ setPlugin: (pluginId: string) => void }>;
+    isReady: boolean;
+    name: string;
+}
+
+interface SettingSectionConfig {
+    id: string;
+    intlLabel: {
+        id: string;
+        defaultMessage: string;
+    };
+}
+
+interface SettingLinkConfig {
+    intlLabel: {
+        id: string;
+        defaultMessage: string;
+    };
+    id: string;
+    to: string;
+    Component: () => Promise<any>;
+}
+
 export default {
     register(app: any) {
         app.customFields.register({
@@ -26,11 +68,25 @@ export default {
         });
 
         app.registerPlugin({
-          id: PLUGIN_ID,
-          initializer: Initializer,
-          isReady: true,
-          name: PLUGIN_ID,
+            id: PLUGIN_ID,
+            initializer: Initializer,
+            isReady: false,
+            name: PLUGIN_ID,
         });
+    },
+
+    bootstrap(app: StrapiAdminAPI) {
+        console.log(`[${PLUGIN_ID}] Bootstrap started`);
+        const plugin = app.getPlugin(PLUGIN_ID);
+
+        if (plugin) {
+            console.log(`[${PLUGIN_ID}] Setting plugin as ready`);
+            plugin.isReady = true;
+        } else {
+            console.warn(`[${PLUGIN_ID}] Plugin not found during bootstrap`);
+        }
+
+        console.log(`[${PLUGIN_ID}] Bootstrap completed`);
     },
 
     async registerTrads(app: any) {
